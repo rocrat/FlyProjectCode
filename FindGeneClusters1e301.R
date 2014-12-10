c13c336ad7ce2bee178213a27906eb586177892b
Dominic LaRoche <dlaroche@email.arizona.edu>
2014-12-10 15:49:32 -0700

    Decided that the clustering algorithm was going to take too long so set e-value cut-off based on ecdf.

--- FindGeneClusters1e301.R
+++ FindGeneClusters1e301.R
@@ -4,26 +4,17 @@ library(linkcomm)
 dat<-read.delim("C:/Projects/FlyEvolution/CombinedHMMER.csv",sep=";",header=TRUE)
 d1<-dim(dat)[1]
 dat<-dat[as.character(dat$id1)!=as.character(dat$id2),]
-#subset data for testing
+#subset data for efficiency
 dat$weight<-1-dat$evalue
-dat1<-dat[sample(dim(dat)[1],10000),]
+#selected the cut-off of 1e-301 based on a plot of the ecdf of e-values
+dat1<-dat[dat$evalue<=1e-301,]
 system.time(lc<-getLinkCommunities(dat1[,c(1,2,4)]))
 rm(dat)
-save.image(file="C:/Projects/FlyEvolution/LinkClusterstest.Rdata")
+save.image(file="C:/Projects/FlyEvolution/LinkClusters1e301.Rdata")
 
 linkmat<-matrix("",length(lc$clusters),1)
 for(i in 1:length(lc$clusters)){
   linkmat[i,1]<-paste(getNodesIn(lc,clusterids=i),collapse=";")
 }
 write.csv(linkmat,"ClusterMatrix.csv",)
-save.image(file="/xdisk/rlapoint/LinkMatrix.Rdata")
-
-plot(lc, type = "graph", layout = layout.fruchterman.reingold,vlabel=F)
-plot(lc, type = "graph", layout = layout.fruchterman.reingold,clusterids=c(728))#731
-
-cdfall<-ecdf(dat$evalue)
-plot(cdf)
-cdfsmall<-ecdf(dat[dat$evalue!=0 & dat$evalue<1e-300,]$evalue)
-plot(cdf)
-
-sum(dat$evalue!=0&dat$evalue<1e-301)
+save.image(file="/xdisk/rlapoint/LinkMatrix1e301.Rdata")
